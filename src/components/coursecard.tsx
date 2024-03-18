ed686fb0bdca47b4d03ab70b17cc34f5d0b0075e
Kariimayman <84875245+Kariimayman@users.noreply.github.com>
2024-03-18 02:48:31 +0200

    fixgap

--- src/components/coursecard.tsx
+++ src/components/coursecard.tsx
@@ -12,8 +12,10 @@ const CourseCard: React.FC<CardProps> = ({ imageUrl, title, price, onClick }) =>
             <div className="mt-6">
                 <h2 className="text-xl font-bold text-black py-2">{title}</h2>
                 <div className="flex justify-between items-center my-5"> {/* Corrected placement */}
-                    <p className='line-through line-through-offset-[10px] decoration-[3px] text-gray-700  font-sans text-xl w-1/5'>${30}</p>
-                    <p className="text-gray-700  py-2 text-2xl -ml-20">${price.toFixed(2)}</p>
+                    <div className="w-1/2 flex flex-row py-2 gap-2">
+                        <p className='line-through line-through-offset-[10px] decoration-[3px] text-gray-700  font-sans text-xl w-1/5'>${30}</p>
+                        <p className="text-gray-700   text-2xl">${price.toFixed(2)}</p>
+                    </div>
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                         onClick={onClick}
