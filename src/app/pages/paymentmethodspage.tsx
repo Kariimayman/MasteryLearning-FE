e1c146b6c2a5bc789e7d8d9d65318f421c4d13a2
Kariimayman <84875245+Kariimayman@users.noreply.github.com>
2024-03-18 03:04:45 +0200

    sss

--- src/app/pages/paymentmethodspage.tsx
+++ src/app/pages/paymentmethodspage.tsx
@@ -54,13 +54,13 @@ export default function PaymentMethodsPage() {
                     <h2 className='text-[#54595f] font-sans text-right text-2xl pb-5'>
                         ﺣﺴﺎب رﻗﻢ
                     </h2>
-                    <h2 className='text-[#54595f] font-sans text-right text-xl pb-10 '>
+                    <h2 className='text-[#54595f] font-sans text-right text-l pb-10 '>
                         130000010006080396987
                     </h2>
                     <h2 className='text-[#54595f] font-sans text-right text-2xl pb-5'>
                         اﻳﺒﺎن
                     </h2>
-                    <h2 className='text-[#54595f] font-sans text-right text-xl pb-10'>
+                    <h2 className='text-[#54595f] font-sans text-right text-l pb-10'>
                         SA2180000130608010396987
                     </h2>
                     <div className=" py-10 bg-black rounded-[100px]">
@@ -88,13 +88,13 @@ export default function PaymentMethodsPage() {
                     <h2 className='text-[#54595f] font-sans text-right text-2xl pb-5'>
                         ﺣﺴﺎب رﻗﻢ
                     </h2>
-                    <h2 className='text-[#54595f] font-sans text-right text-xl pb-10 '>
+                    <h2 className='text-[#54595f] font-sans text-right text-l pb-10 '>
                         005623690001
                     </h2>
                     <h2 className='text-[#54595f] font-sans text-right text-2xl pb-5'>
                         اﻳﺒﺎن
                     </h2>
-                    <h2 className='text-[#54595f] font-sans text-right text-xl pb-10'>
+                    <h2 className='text-[#54595f] font-sans text-right text-l pb-10'>
                         EG650019050100000005623690001
                     </h2>
                     <div className="py-10 bg-black rounded-[100px]">
@@ -117,13 +117,13 @@ export default function PaymentMethodsPage() {
                     <h2 className='text-[#54595f] font-sans text-right text-2xl pb-5'>
                         ﺣﺴﺎب رﻗﻢ
                     </h2>
-                    <h2 className='text-[#54595f] font-sans text-right text-xl pb-10 '>
+                    <h2 className='text-[#54595f] font-sans text-right text-l pb-10 '>
                         20872589000102
                     </h2>
                     <h2 className='text-[#54595f] font-sans text-right text-2xl pb-5'>
                         اﻳﺒﺎن
                     </h2>
-                    <h2 className='text-[#54595f] font-sans text-right text-xl pb-10'>
+                    <h2 className='text-[#54595f] font-sans text-right text-l pb-10'>
                         SA4710000020872589000102
                     </h2>
                     <div className="py-10 bg-black rounded-[100px]">
@@ -191,7 +191,7 @@ export default function PaymentMethodsPage() {
                     </div>
                 </div>
 
-                <div className="slide px-10 py-10 bg-white rounded-[100px] shadow-2xl outline outline-2 outline-black">
+                <div className="slide px-10 py-10 bg-white rounded-[100px] shadow-2xl outline outline-2 outline-black ">
                     <h1 className='text-[#1BBBA4] font-sans text-right text-3xl pb-10'>
                         ﻣﻮﻧﻲ ﺟﺮام
                     </h1>
@@ -220,8 +220,8 @@ export default function PaymentMethodsPage() {
                     </div>
                 </div>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 py-5 px-20 bg-[#1BBBA4]">
-                <div className="slide px-10 py-10 bg-white rounded-[100px] shadow-2xl outline outline-2 outline-black">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 py-5 px-10 bg-[#1BBBA4]">
+                <div className="slide w-full px-10 py-10 bg-white rounded-[100px] shadow-2xl outline outline-2 outline-black">
                     <h1 className='text-[#1BBBA4] font-sans text-right text-3xl pb-10'>
 
                         ﺑﻄﺎﻗﺔ اﺋﺘﻤﺎن
--- src/components/textwithbutton.tsx
+++ src/components/textwithbutton.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const TextWithButton = () => {
     return (
         <div className="flex flex-col items-center justify-center pt-20">
-            <p className="sm:text-xl md:text-2xl lg:text-4xl text-blue-800 pt-5 font-light font-sans pl-20  w-full">_______________________________________________</p>
+            <p className="sm:text-xl md:text-2xl lg:text-4xl text-blue-800 pt-5 font-light font-sans   w-full">_______________________________________________</p>
 
             <p className="text-center text-4xl text-blue-800 pt-5 font-medium font-sans">المعرفة قوة، والتعلُم هو ضمانك للحرية</p>
-            <p className="text-right sm:text-xl md:text-2xl lg:text-4xl text-blue-800 pt-5 font-light font-sans pr-20 w-full">_______________________________________________</p>
+            <p className="text-right sm:text-xl md:text-2xl lg:text-4xl text-blue-800 pt-5 font-light font-sans  w-full">_______________________________________________</p>
             <p className="text-center text-4xl text-blue-800 pt-5 font-normal font-sans pb-5"> عزز مهاراتك، وحرر حياتك</p>
 
             <button onClick={() => (location.href = "/ProgramsPage")} className="px-10 py-5 pb-7  my-10 text-3xl mt-4 bg-gradient-to-r font-sans hover:from-green-500 hover:to-blue-500 from-green-600 to-blue-600 text-white rounded-full shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
